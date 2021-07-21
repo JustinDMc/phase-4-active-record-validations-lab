dff6f3f0dac5330276b6980b8d3da74d0cde0701
Justin <jdanielmcarthur@msn.com>
2021-07-20 17:01:40 -0700

    commit tree bb5635885a677614b034d7cda7a2569570d1cb0b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validate :title_is_clickbait
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    # validate :title_is_clickbait
+
+    def title_is_clickbait
+        if title && !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+        # unless title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "Not clickbait-y enough!")
+        end
+    end
+
 end
